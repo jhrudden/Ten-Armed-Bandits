20d3b5e1a834fa8d366bce07f2b240cda464ca63
John Henry Rudden <johnhenryrudden@gmail.com>
2024-01-25 22:43:25 -0500

    add unstationary environments

--- env.py
+++ env.py
@@ -5,14 +5,16 @@ from typing import Tuple, Optional
 class BanditEnv:
     """Multi-armed bandit environment"""
 
-    def __init__(self, k: int, random_state: Optional[float] = None) -> None:
+    def __init__(self, k: int, random_state: Optional[float] = None, stationary: bool = True) -> None:
         """__init__.
 
         Args:
             k (int): number of arms/bandits
             random_state (float): random seed
+            stationary (bool): whether the environment is stationary
         """
         self.k = k
+        self.stationary = stationary
         
         if random_state is not None:
             np.random.seed(random_state)
@@ -23,8 +25,12 @@ class BanditEnv:
         """Resets the mean payout/reward of each arm.
         This function should be called at least once after __init__()
         """
-        # Initialize means of each arm distributed according to standard normal
-        self.means = np.random.normal(size=self.k)
+        if self.stationary:
+            # Initialize means of each arm distributed according to standard normal
+            self.means = np.random.normal(size=self.k)
+        else:
+            # Initialize means of each arm to 0
+            self.means = np.zeros(self.k)
 
     def step(self, action: int) -> Tuple[float, int]:
         """Take one step in env (pull one arm) and observe reward
@@ -33,5 +39,8 @@ class BanditEnv:
             action (int): index of arm to pull
         """
         reward = np.random.normal(loc=self.means[action])
+
+        if not self.stationary:
+            self.means += np.random.normal(size=self.k, scale=0.01)
 
         return reward
