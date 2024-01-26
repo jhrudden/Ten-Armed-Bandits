a4f231cae039df7a8373f947b420a58797c1b360
John Henry Rudden <johnhenryrudden@gmail.com>
2024-01-25 22:43:08 -0500

    add UCB algo + unstationary logic

--- agent.py
+++ agent.py
@@ -22,7 +22,7 @@ class BanditAgent(ABC):
     N = None # Number of times each arm was pulled
     t = None # Number of time steps taken
 
-    def __init__(self, k: int, init: int, step_size: float) -> None:
+    def __init__(self, k: int, init: int, step_size: float, verbose: bool = False) -> None:
         """Abstract bandit agent class
 
         Implements common functions for both epsilon greedy and UCB
@@ -31,10 +31,12 @@ class BanditAgent(ABC):
             k (int): number of arms
             init (init): initial value of Q-values
             step_size (float): step size
+            verbose (bool): include additional information in string representation
         """
         self.k = k
         self.init = init
         self.step_size = step_size
+        self.verbose = verbose
         self.reset()
 
     def reset(self) -> None:
@@ -63,7 +65,7 @@ class BanditAgent(ABC):
 
 class EpsilonGreedy(BanditAgent):
     def __init__(
-        self, k: int, init: int, epsilon: float, step_size: Optional[float] = None
+        self, k: int, init: int, epsilon: float, step_size: Optional[float] = None, verbose: bool = False
     ) -> None:
         """Epsilon greedy bandit agent
 
@@ -72,8 +74,9 @@ class EpsilonGreedy(BanditAgent):
             init (init): initial value of Q-values
             epsilon (float): random action probability
             step_size (float or None): step size. If None, then it is equal to 1 / N_t (dynamic step size)
+            verbose (bool): include additional information in string representation
         """
-        super().__init__(k, init, step_size)
+        super().__init__(k, init, step_size, verbose)
         self.epsilon = epsilon
 
     def choose_action(self):
@@ -115,11 +118,15 @@ class EpsilonGreedy(BanditAgent):
         self.Q[action] += step_ceof * (reward - self.Q[action])
 
     def __str__(self) -> str:
-        return f"$\epsilon = {self.epsilon}$"
-
+        stringified = r"$\epsilon$-greedy $\epsilon = {}$ $Q_1 = {}$".format(self.epsilon, self.init)
+        if self.step_size is not None:
+            stringified += r" $\alpha = {}$".format(self.step_size)
+        elif self.verbose:
+            stringified += r" $step\_size = 1 / N_t$"
+        return stringified
 
 class UCB(BanditAgent):
-    def __init__(self, k: int, init: int, c: float, step_size: float) -> None:
+    def __init__(self, k: int, init: int, c: float, step_size: float, verbose: bool = False) -> None:
         """Epsilon greedy bandit agent
 
         Args:
@@ -127,8 +134,10 @@ class UCB(BanditAgent):
             init (init): initial value of Q-values
             c (float): UCB constant that controls degree of exploration
             step_size (float): step size (use constant step size in case of UCB)
+            verbose (bool): include additional information in string representation
         """
-        super().__init__(k, init, step_size)
+        super().__init__(k, init, step_size, verbose)
+        assert step_size is not None, "UCB requires constant step size"
         self.c = c
 
     def choose_action(self):
@@ -136,8 +145,15 @@ class UCB(BanditAgent):
 
         Use UCB action selection. Be sure to consider the case when N_t = 0 and break ties randomly (use argmax() from above)
         """
-        # TODO
         action = None
+
+        best_action_estimates = np.zeros(self.k)
+        for a in range(self.k):
+            if self.N[a] == 0:
+                best_action_estimates[a] = np.inf
+            else:
+                best_action_estimates[a] = self.Q[a] + self.c * np.sqrt(np.log(self.t) / self.N[a])
+        action = argmax(best_action_estimates)
         return action
 
     def update(self, action: int, reward: float) -> None:
@@ -149,6 +165,15 @@ class UCB(BanditAgent):
         """
         self.t += 1
 
-        # TODO update self.N
+        self.N[action] += 1
 
-        # TODO update self.Q
+        self.Q[action] += self.step_size * (reward - self.Q[action])
+    
+    def __str__(self) -> str:
+        stringified = r"UCB $c = {}$".format(self.c)
+        if self.step_size is not None:
+            stringified += r" $\alpha = {}$".format(self.step_size)
+        elif self.verbose:
+            stringified += r" $step\_size = 1 / N_t$"
+        
+        return stringified
